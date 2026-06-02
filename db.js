50378f6a0b4a4d04938b8acf07889b9bfc6e324a
raheelkhan1122 <153066238+raheelkhan1122@users.noreply.github.com>
2026-06-03 01:58:15 +0500

    Refactor MySQL connection to use connection pool

--- db.js
+++ db.js
@@ -1,15 +1,23 @@
 const mysql = require('mysql2');
 
-const db = mysql.createConnection({
+const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
   password: '',
-  database: 'freelance_project_tracking'
+  database: 'freelance_project_tracking',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-db.connect((err) => {
-  if (err) throw err;
-  console.log('MySQL connected');
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error('MySQL connection failed:', err.message);
+    console.error('Make sure MySQL is running and the database exists.');
+    return;
+  }
+  console.log('MySQL connected successfully');
+  connection.release();
 });
 
-module.exports = db;
+module.exports = pool;
